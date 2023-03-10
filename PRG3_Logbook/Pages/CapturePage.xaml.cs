1dc2ec798bfb67254ff8ad1c79780775aad0a38a
athavan94 <athavan94@gmail.com>
2023-03-10 15:47:29 +0100

    Add form and save function

--- PRG3_Logbook/Pages/CapturePage.xaml.cs
+++ PRG3_Logbook/Pages/CapturePage.xaml.cs
@@ -7,4 +7,30 @@ public partial class CapturePage : ContentPage
 		InitializeComponent();
 	}
 
+	private async void OnSaveClicked(object sender, EventArgs e)
+	{
+		// Get values from input fields
+		string dateFrom = DatePickerFrom.Date.ToString("dd.MM.yyyy");
+		string dateUntil = DatePickerUntil.Date.ToString("dd.MM.yyyy");
+		double kilometre = 0;
+
+		// Check if kilometre is double
+		if (!double.TryParse(KilometerEntry.Text, out kilometre))
+		{
+			await DisplayAlert("Error", "Enter a valid kilometre value.", "OK");
+			return;
+		}
+
+		// Create path and filename for the file
+		string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+		string filename = Path.Combine(documentsPath, "Logbook.txt");
+
+		// Create text for the file
+		string fileText = $"Date from: {dateFrom}\nDate until: {dateUntil}\nKilometre: {kilometre}\n\n";
+
+		// Add text to file
+		File.AppendAllText(filename, fileText );
+
+		await DisplayAlert("Success", "Die Daten wurden erfolgreich gespeichert.", "OK");
+	}
 }
