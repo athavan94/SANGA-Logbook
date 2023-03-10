7f311b4860d17745ef2754c877cfcff5a1ce67dc
athavan94 <athavan94@gmail.com>
2023-03-10 14:39:38 +0100

    Add capture and statistic page. Add icons and change color

---
+++ PRG3_Logbook/Capture.xaml.cs
@@ -0,0 +1,9 @@
+namespace PRG3_Logbook;
+
+public partial class Capture : ContentPage
+{
+	public Capture()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PRG3_Logbook/Statistic.xaml.cs
@@ -0,0 +1,9 @@
+namespace PRG3_Logbook;
+
+public partial class Statistic : ContentPage
+{
+	public Statistic()
+	{
+		InitializeComponent();
+	}
+}
